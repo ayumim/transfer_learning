adaec2e50eae88f7a1a894e53523220efb4cd4ba
Ayumi Mutoh <amutoh@ncsu.edu>
2025-12-18 16:36:23 -0500

    fix bugs

--- test.R
+++ test.R
@@ -1,23 +1,21 @@
 library(mvtnorm)
+library(invgamma)
 
 p <- 10
-n_t <- 10
+n_t <- 100
 sigma2 <- 1
 # theta_s <- sample(c(runif(p, -6, -3),runif(p, 3, 6)), p)
 theta_s <- ifelse(runif(p)<0.5, runif(p, -6, -3), runif(p, 3, 6))
 
-kappa2 <- diag(10, p)
+kappa2 <- 10
 theta_t <- rnorm(p, mean = theta_s, sd = sqrt(kappa2))
 
 x_t <- rmvnorm(n_t, rep(0, p), diag(1, p))
-
-
-
 y <- rnorm(n_t, mean=x_t %*%theta_t, sd=sqrt(sigma2))
 
 f_i <- c(x_t %*% theta_s)
 
-gibbs <- function(y, x, N=10000, tau2=1, a_gamma=2, b_gamma=2, 
+gibbs <- function(y, x, f_i, N=10000, tau2=1, a_gamma=2, b_gamma=2, 
                   a_sigma=2, b_sigma=2) {
   n <- length(y)
   p <- ncol(x)
@@ -42,8 +40,22 @@ gibbs <- function(y, x, N=10000, tau2=1, a_gamma=2, b_gamma=2,
     for(j in 1:n) {
       v <- 1 / (1/sigma_prev + 1/gamma_prev)
       m <- (y[j] - f_i[j]) / sigma_prev + delta_prev / gamma_prev
-      alpha[i-1, j] <- rnorm(1, mean=m * v, sd=sqrt(v))
+      alpha[i, j] <- rnorm(1, mean=m * v, sd=sqrt(v))
     }
     
+    # delta
+    v_delta <- 1 / (n / gamma_prev + 1 / tau2)
+    m_delta <- (sum(alpha[i,])/gamma_prev) * v_delta 
+    delta[i] <- rnorm(1, mean=m_delta, sd=sqrt(v_delta))
+    
+    # gamma
+    shape_g <- a_gamma + n / 2
+    rate_g <- sum((alpha[i,] - delta[i])^2) / 2 + b_gamma
+    gamma[i] <- rinvgamma(1, shape=shape_g, rate=rate_g)
+    
+    # sigma 
+    shape_s <- a_sigma + n / 2
+    rate_s <- sum((y - (alpha[i,] + f_i))^2) / 2 + b_sigma
+    sigma[i] <- rinvgamma(1, shape = shape_s, rate=rate_s)
   }
 }
